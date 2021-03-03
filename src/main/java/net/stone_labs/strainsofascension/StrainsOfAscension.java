653bb9dbca45973a354128c7d9a99b9b430d2ffa
StoneLabs <levyehrstein@googlemail.com>
2021-03-03 17:31:30 +0100

    Fix balancing, nether, and minor stuff

--- src/main/java/net/stone_labs/strainsofascension/StrainsOfAscension.java
+++ src/main/java/net/stone_labs/strainsofascension/StrainsOfAscension.java
@@ -2,29 +2,19 @@ package net.stone_labs.strainsofascension;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.client.realms.dto.RealmsServer;
-import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.LiteralText;
 import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldSaveHandler;
-import net.minecraft.world.dimension.DimensionType;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.jmx.Server;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 import static net.stone_labs.strainsofascension.StrainsOfAscensionData.horrorMessages;
@@ -36,18 +26,20 @@ L   H  DH	    Mining Fatigue  Weakness   Hunger  Slowness    Blindness    Nausea
 2   24	16	    0	            0
 3   8	16	    0	            0	        0
 4   -8	16	    0	            0	        1	    0
-5   -24	16	    0	            1	        2	    0	        0
-6   -32	8	    0	            2           3	    1	        0                                  + INSANITY
-7   -48	16	    1	            2	        4	    2	        0	        0	    0              + INSANITY
-8   -59	11	    3	            2	        5	    2	        0	        0	    0	    0      + INSANITY
+5   -24	16	    0	            1	        2	    0	        (0)
+6   -32	8	    0	            2           3	    1	        (0)                                   + INSANITY
+7   -48	16	    1	            2	        4	    2	         0	        0R	    0R                + INSANITY
+8   -59	11	    1               2	        5	    2	         0	        0R	    1R	    1R        + INSANITY
+NETHER          1               3           6       2           (0)         0R      2R      2R        + INSANITY
 
 DURATION 120*20
  */
 
 public class StrainsOfAscension implements DedicatedServerModInitializer
 {
-    public final static int effectDuration = 120*20+10;
-    public final static int effectDurationBlindness = 3*10+10;
+    public final static int effectDuration = 120 * 20 + 10;
+    public final static int effectDurationBlindness = 3 * 10 + 10;
+    public final static float effectRandomProbability = 1.0f/effectDuration;
     public final static boolean showIcon = false;
 
     public static void setEffectLayer1(ServerPlayerEntity player)
@@ -94,27 +86,50 @@ public class StrainsOfAscension implements DedicatedServerModInitializer
         setEffectNightVisionBlindness(player, true);
     }
 
-    public static void setEffectLayer7(ServerPlayerEntity player)
+    public static void setEffectLayer7(ServerPlayerEntity player, Random random)
     {
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, effectDuration, 1, true, false, showIcon));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, effectDuration, 2, true, false, showIcon));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, effectDuration, 4, true, false, showIcon));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, effectDuration, 2, true, false, showIcon));
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, effectDuration, 0, true, false, showIcon));
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, effectDuration, 0, true, false, showIcon));
+        if (random.nextFloat() < effectRandomProbability)
+        {
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, effectDuration, 0, true, false, showIcon));
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, effectDuration, 0, true, false, showIcon));
+        }
         setEffectNightVisionBlindness(player, false);
     }
 
-    public static void setEffectLayer8(ServerPlayerEntity player)
+    public static void setEffectLayer8(ServerPlayerEntity player, Random random)
     {
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, effectDuration, 2, true, false, showIcon));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, effectDuration, 1, true, false, showIcon));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, effectDuration, 2, true, false, showIcon));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, effectDuration, 5, true, false, showIcon));
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, effectDuration, 2, true, false, showIcon));
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, effectDuration, 0, true, false, showIcon));
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, effectDuration, 0, true, false, showIcon));
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, effectDuration, 0, true, false, showIcon));
+        if (random.nextFloat() < effectRandomProbability)
+        {
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, effectDuration, 0, true, false, showIcon));
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, effectDuration, 1, true, false, showIcon));
+        }
+        if (random.nextFloat() < effectRandomProbability)
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, effectDuration, 1, true, false, showIcon));
         setEffectNightVisionBlindness(player, false);
+    }
+
+    public static void setEffectLayerNether(ServerPlayerEntity player, Random random)
+    {
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.MINING_FATIGUE, effectDuration, 1, true, false, showIcon));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, effectDuration, 3, true, false, showIcon));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, effectDuration, 6, true, false, showIcon));
+        player.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, effectDuration, 2, true, false, showIcon));
+        if (random.nextFloat() < effectRandomProbability)
+        {
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, effectDuration, 0, true, false, showIcon));
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, effectDuration, 2, true, false, showIcon));
+        }
+        if (random.nextFloat() < effectRandomProbability)
+            player.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, effectDuration, 2, true, false, showIcon));
+        setEffectNightVisionBlindness(player, true);
     }
 
     public static void setEffectNightVisionBlindness(ServerPlayerEntity player, boolean allowNVCancel)
@@ -134,42 +149,63 @@ public class StrainsOfAscension implements DedicatedServerModInitializer
         player.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, effectDurationBlindness, 0, true, false, showIcon));
     }
 
+    public static void playRandomHorrorMessage(ServerPlayerEntity player, Random random)
+    {
+        player.sendMessage(new LiteralText(horrorMessages.get(random.nextInt(horrorMessages.size()))), false);
+    }
+
+    public static void playRandomHorrorAudio(ServerPlayerEntity player, Random random)
+    {
+        player.playSound(horrorSounds.get(random.nextInt(horrorSounds.size())), SoundCategory.AMBIENT, random.nextFloat(), 0);
+    }
+
     private static void setEffects(double height, ServerPlayerEntity player, Random random)
     {
-        if (player.world.getRegistryKey() != World.OVERWORLD)
+        if (player.interactionManager.getGameMode() == GameMode.SPECTATOR
+                || player.interactionManager.getGameMode() == GameMode.CREATIVE)
             return;
 
-        if (player.interactionManager.getGameMode() == GameMode.SPECTATOR)
-            return;
+        if (player.world.getRegistryKey() == World.OVERWORLD)
+        {
+            if (height >= 40)
+                return; // You can live... for now...
 
-        if (height >= 40)
-            return; // You can live... for now...
+            // Do not spam messages and audio every tick
+            if (height < -64)
+                height = -64;
 
-        // Do not spam messages and audio every tick
-        if (height < -64)
-            height = -64;
+            if (height < -59)
+                setEffectLayer8(player, random);
+            else if (height < -48)
+                setEffectLayer7(player, random);
+            else if (height < -32)
+                setEffectLayer6(player);
+            else if (height < -24)
+                setEffectLayer5(player);
+            else if (height < -8)
+                setEffectLayer4(player);
+            else if (height < 8)
+                setEffectLayer3(player);
+            else if (height < 24)
+                setEffectLayer2(player);
+            else if (height < 40)
+                setEffectLayer1(player);
 
-        if (height < -59)
-            setEffectLayer8(player);
-        else if (height < -48)
-            setEffectLayer7(player);
-        else if (height < -32)
-            setEffectLayer6(player);
-        else if (height < -24)
-            setEffectLayer5(player);
-        else if (height < -8)
-            setEffectLayer4(player);
-        else if (height < 8)
-            setEffectLayer3(player);
-        else if (height < 24)
-            setEffectLayer2(player);
-        else if (height < 40)
-            setEffectLayer1(player);
+            if (height < -32 && random.nextFloat() < ((-0.0001f) * height - 0.0032f))
+                playRandomHorrorMessage(player, random);
+            if (height < -32 && random.nextFloat() < ((-0.0002f) * height - 0.0064f))
+                playRandomHorrorAudio(player, random);
+        }
+        else if (player.world.getRegistryKey() == World.NETHER)
+        {
+            setEffectLayerNether(player, random);
 
-        if (height < -32 && random.nextFloat() < ((-0.0001f) * height - 0.0032f))
-            player.sendMessage(new LiteralText(horrorMessages.get(random.nextInt(horrorMessages.size()))), false);
-        if (height < -32 && random.nextFloat() < ((-0.0002f) * height - 0.0064f))
-            player.playSound(horrorSounds.get(random.nextInt(horrorSounds.size())), SoundCategory.AMBIENT, random.nextFloat(), 0);
+            if (random.nextFloat() < 0.0032f)
+                playRandomHorrorMessage(player, random);
+            if (random.nextFloat() < 0.0064f)
+                playRandomHorrorAudio(player, random);
+
+        }
     }
 
     public static class ServerTickEvent implements ServerTickEvents.EndTick
