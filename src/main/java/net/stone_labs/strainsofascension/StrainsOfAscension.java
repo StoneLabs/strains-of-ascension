6aa35454dda687038c18508298d39ede7f86b41c
StoneLabs <levyehrstein@googlemail.com>
2021-03-03 18:41:31 +0100

    Upversion for 2.1.0 release

--- src/main/java/net/stone_labs/strainsofascension/StrainsOfAscension.java
+++ src/main/java/net/stone_labs/strainsofascension/StrainsOfAscension.java
@@ -224,7 +224,7 @@ public class StrainsOfAscension implements DedicatedServerModInitializer
 
     public static final String MOD_ID = "strainsofascension";
     public static final String MOD_NAME = "Strains of Ascension";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "2.1.0";
 
     @Override
     public void onInitializeServer()
