5159f01062d89757047b3c48896e4f223a0c4997
StoneLabs <levyehrstein@googlemail.com>
2021-07-22 02:28:52 +0200

    Add VexBoss spawn to BellMixin

--- src/main/java/net/stone_labs/strainsofascension/mixin/BellBlockMixin.java
+++ src/main/java/net/stone_labs/strainsofascension/mixin/BellBlockMixin.java
@@ -3,12 +3,8 @@ package net.stone_labs.strainsofascension.mixin;
 import net.minecraft.block.BellBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FlintAndSteelItem;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.item.MilkBucketItem;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.LiteralText;
@@ -17,17 +13,22 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.stone_labs.strainsofascension.effects.BlindnessStrain;
+import net.minecraft.world.explosion.Explosion;
+import net.stone_labs.strainsofascension.VexBossEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 @Mixin(BellBlock.class)
 abstract class BellBlockMixin
 {
+    private Random random = new Random();
+
     @Inject(at = @At("RETURN"), method = "onUse")
     private void init(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> info)
     {
@@ -37,10 +38,22 @@ abstract class BellBlockMixin
         for (int x = -2; x <= 2; x++)
             for (int y = -1; y <= 1; y++)
                 for (int z = -2; z <= 2; z++)
-                    if (world.getBlockState(pos.add(x, y, z)).isOf(Blocks.SPAWNER))
+                {
+                    BlockPos spawner = pos.add(x, y, z);
+                    if (world.getBlockState(spawner).isOf(Blocks.SPAWNER))
                     {
-                        player.sendMessage(new LiteralText("Test"), false);
+                        if (random.nextFloat() < 0.1f)
+                        {
+                            new VexBossEntity((ServerWorld) world, spawner);
+                            world.createExplosion(null, spawner.getX() + 0.5, spawner.getY() + 0.5, spawner.getZ() + 0.5, 1, Explosion.DestructionType.BREAK);
+                            world.breakBlock(spawner, false);
+
+                            world.createExplosion(null, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 1, Explosion.DestructionType.BREAK);
+                            world.breakBlock(pos, false);
+                        }
+
                         return;
                     }
+                }
     }
 }
