73c6dfbfd230f03ad7d089ffe66efc40343966ce
StoneLabs <levyehrstein@googlemail.com>
2021-07-28 14:46:24 +0200

    Fix talismans not having effect in ascension mode

--- src/main/java/net/stone_labs/strainsofascension/effects/strains/PoisonNauseaStrain.java
+++ src/main/java/net/stone_labs/strainsofascension/effects/strains/PoisonNauseaStrain.java
@@ -3,11 +3,11 @@ package net.stone_labs.strainsofascension.effects.strains;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.stone_labs.strainsofascension.artifacts.ArtifactState;
 import net.stone_labs.strainsofascension.StrainManager;
 import net.stone_labs.strainsofascension.artifacts.Artifacts;
 import net.stone_labs.strainsofascension.effects.BasicEffect;
-import net.stone_labs.strainsofascension.effects.Effect;
 
 import java.util.Random;
 
@@ -27,9 +27,14 @@ public class PoisonNauseaStrain extends BasicEffect
         if (layer < 7)
             return;
 
-        double artifactMultiplier = Math.max(1 - 0.1 * artifactState.GetPower(Artifacts.POISON_BONUS), 0.4);
-        if (StrainManager.strainMode == StrainManager.STRAINMODE.ASCENSION ||
-                random.nextFloat() < StrainManager.effectRandomProbability * frequency * artifactMultiplier)
+        // Expected number of jumps till getting poison effect in ascension mode (~5 ticks / jump):
+        // jumps = 1/(1 - (1 - 1.0/(10*x + 1))^5) for x elem {0, 6}
+        double probabilitySTV =
+                StrainManager.strainMode == StrainManager.STRAINMODE.ASCENSION ?
+                        1.0 / (10 * artifactState.GetPower(Artifacts.POISON_BONUS) + 1) :
+                        StrainManager.effectRandomProbability * frequency * (1 - 0.1 * artifactState.GetPower(Artifacts.POISON_BONUS));
+
+        if (random.nextFloat() < probabilitySTV)
         {
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.POISON, StrainManager.effectDuration, 0, true, false, StrainManager.showIcon));
             if (doNausea)
--- src/main/java/net/stone_labs/strainsofascension/effects/strains/WitherStrain.java
+++ src/main/java/net/stone_labs/strainsofascension/effects/strains/WitherStrain.java
@@ -26,11 +26,14 @@ public class WitherStrain extends BasicEffect
         if (layer < 8)
             return;
 
-        double artifactMultiplier = Math.max(1 - 0.1 * artifactState.GetPower(Artifacts.WITHER_BONUS), 0.4);
-        if (StrainManager.strainMode == StrainManager.STRAINMODE.ASCENSION ||
-                random.nextFloat() < StrainManager.effectRandomProbability * frequency * artifactMultiplier)
-        {
+        // Expected number of jumps till getting wither effect in ascension mode (~5 ticks / jump):
+        // jumps = 1/(1 - (1 - 1.0/(10*x + 1))^5) for x elem {0, 6}
+        double probabilitySTV =
+                StrainManager.strainMode == StrainManager.STRAINMODE.ASCENSION ?
+                        1.0 / (10 * artifactState.GetPower(Artifacts.WITHER_BONUS) + 1) :
+                        StrainManager.effectRandomProbability * frequency * (1 - 0.1 * artifactState.GetPower(Artifacts.WITHER_BONUS));
+
+        if (random.nextFloat() < probabilitySTV)
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.WITHER, StrainManager.effectDuration, layer - 7, true, false, StrainManager.showIcon));
-        }
     }
 }
